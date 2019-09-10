f7c9dd7c00d9f329b93cdf3f57da0e7acc162a42
Oscar Shoudt <shoudto@gmail.com>
2019-09-10 18:01:59 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,2 +1,15 @@
 class Artist < ActiveRecord::Base
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS artists (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    genre TEXT,
+    age INTEGER,
+    hometown TEXT
+    )
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
 end
